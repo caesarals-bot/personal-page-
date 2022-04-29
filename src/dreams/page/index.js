602b5392a5b71c228a618ca1166579feab18b375
cesar londoño <slac_@hotmail.com>
2022-04-28 21:08:09 -0400

    index logo name

--- src/dreams/page/index.js
+++ src/dreams/page/index.js
@@ -1,5 +1,8 @@
+export { Skills } from "./skills/Skills";
+export { Contactame } from "./contactame/Contactame";
 export {AboutUs} from "./about/AboutUs";
 export {BlogPage} from "./blog/BlogPage";
 export {HomeDreams} from "./home/HomeDreams";
 export {GamePage} from "./play/GamePage";
+export {QuienSoy} from "./yo/QuienSoy";
 
